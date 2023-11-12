9da171d47e6c0d244295f45a15aa3484d3946bba
unknown <sebastian.stuehler@fau.de>
2023-11-12 20:38:34 +0100

    Wrote some unittests for the ChooseRandomOperation and the PerformComputation method.

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,37 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import ComputeRandomInteger, ChooseRandomOperation, PerformComputation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def TestComputeRandomInteger(self):
         # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+        MinValue = 1
+        MaxValue = 10
+        for Idx in range(1000):  # Test a large number of random values
+            rand_num = ComputeRandomInteger(MinValue, MaxValue)
+            self.assertTrue(MinValue <= rand_num <= MaxValue)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def TestChooseRandomOperation(self):
+        Operations = ('+', '-', '*')
+        # Check whether the ChooseRandomOperation method selects different
+        # operations.
+        for Idx in range(1000):
+             RandomOperator = ChooseRandomOperation()
+             self.assertIn(RandomOperator, Operations)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+    def TestPerformComputation(self):
+            TestCases = [
+                (5, 2, '+', '5 + 2', 7), (7, 2, '-', '7 - 2', 5),
+                (3, 3, '*', '3 * 3', 9), (9, 4, '*', '9 * 4', 36),
+                (6, 1, '-', '6 - 1', 5), (4, 4, '+', '4 + 4', 8),
             ]
-
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+            # Compare the results of the PerformComputation method with the 
+            # results stored in TestCases
+            for FirstNumber, SecondNumber, Operator, ExpectedProblem, ExpectedAnswer in TestCases:
+                Problem, Answer = PerformComputation(FirstNumber, SecondNumber, Operator)
+                self.assertEqual(Problem, ExpectedProblem)
+                self.assertEqual(Answer, ExpectedAnswer)
 
 if __name__ == "__main__":
     unittest.main()
