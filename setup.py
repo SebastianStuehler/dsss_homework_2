ff5b4338357eee1ef6f647aa14716511dda44af4
unknown <sebastian.stuehler@fau.de>
2023-11-13 08:21:12 +0100

    Included some setup parameters in the setup.py file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsss_homework_2',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    test_suite='unittest',
+)
